efb32cba34522b4f3c313baeda5e25b5b7403442
BadRobotXIII <154725144+BadRobotXIII@users.noreply.github.com>
2025-01-17 22:56:07 -0600

    Commenting

--- htmx/index.js
+++ htmx/index.js
@@ -1,10 +1,11 @@
 
-
+//Change text color on mouse over
 function mouseOver() {
     var element = document.getElementById('tb-cell-icon-connected')
     element.classList.replace("text-gray-400", "text-green-400")
 }
 
+//Change text color on mouse leave
 function mouseLeave() {
     var element = document.getElementById('tb-cell-icon-connected')
     element.classList.replace("text-green-400", "text-gray-400")
