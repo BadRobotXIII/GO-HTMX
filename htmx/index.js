b24559f4375847c12a12c342cdd53dfc2e69b5cd
BadRobotXIII <154725144+BadRobotXIII@users.noreply.github.com>
2025-01-19 23:39:00 -0600

    JavaScript animatiions and interations

--- htmx/index.js
+++ htmx/index.js
@@ -1,18 +1,30 @@
 
 //Change text color on mouse over
+// function mouseOver() {
+//     var element = document.getElementById('tb-cell-icon-connected')
+//     element.classList.replace("text-gray-400", "text-green-400")
+//     animate()
+//     console.log("animate")
+// }
+
+
+// Change text color on mouse leave
+// function mouseLeave() {
+//     var element = document.getElementById('tb-cell-icon-connected')
+//     element.classList.replace("text-green-400", "text-gray-400")
+// }
+
 function mouseOver() {
     var element = document.getElementById('tb-cell-icon-connected')
     element.classList.replace("text-gray-400", "text-green-400")
-    animate()
-    console.log("animate")
+    console.log(element)
 }
 
-//Change text color on mouse leave
 function mouseLeave() {
     var element = document.getElementById('tb-cell-icon-connected')
     element.classList.replace("text-green-400", "text-gray-400")
-}
 
+}
 
 //Animate connected SVG. Rotation and color
 function animate(){
@@ -29,10 +41,8 @@ function animate(){
         secondLast = second
         if (second % 2 == true){
             element.classList.replace("text-gray-400", "text-green-400")
-            console.log('Even set green')
         }else{
             element.classList.replace("text-green-400", "text-gray-400")
-            console.log('Odd set gray')
         }
     }
 }
